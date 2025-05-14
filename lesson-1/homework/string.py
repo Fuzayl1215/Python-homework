aa36834bba1b000eb107a9e9f886f47a979a5963
Fuzayl1215 <fuzaylibrohimov1215@gmail.com>
2025-05-14 14:37:04 +0500

    Create string.py

---
+++ lesson-1/homework/string.py
@@ -0,0 +1,32 @@
+# 1.
+side = 6
+
+perimetr = side * 4
+area = side * side 
+
+print(perimetr, area)
+
+# 2.
+diametr = 4
+
+length = diametr * 3.14
+
+print(length)
+
+# 3.
+a = 5
+b = 7
+mean = (a+b)/2
+
+print(mean)
+
+# 4.
+a = 6
+b = 8
+
+summa = sum([a, b])
+product = a * b
+square1 = a ** 2
+square2 = b ** 2
+
+print(summa, product, square1, square2)
